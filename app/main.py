c3541b7737260fa59a92bc2e5c5de58fa6f67e41
Teashrock <kajitsu22@gmail.com>
2026-07-07 20:24:04 +0300

    Flake8

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         raw_timestamp = datetime.now()
         timestamp = str(raw_timestamp).split(".")[0]
